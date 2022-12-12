b2734a18d11b169e8b29736447ce6b022df5955e
jordan <jordan.lambert.dev@gmail.com>
2022-12-11 19:09:17 -0600

    update to vscode intelesense paths

--- main.c
+++ main.c
@@ -9,8 +9,10 @@
 #include <assert.h>
 #include "FreeRTOS.h"
 #include "task.h"
-#include "pico/stdlib.h"
-#include "pico/cyw43_arch.h"
+
+#include "stdlib.h"
+#include "cyw43_arch.h"
+#include "pico_w.h"
 
 void led_task( void * ptr )
 {
